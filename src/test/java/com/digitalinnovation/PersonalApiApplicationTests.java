a18b4b923d201caa5b32619f94cdd956e1bccb80
daniela.machado <daniela.machado@tacom.com.br>
2020-11-14 21:33:41 -0300

    primeiro comit

---
+++ src/test/java/com/digitalinnovation/PersonalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalinnovation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
